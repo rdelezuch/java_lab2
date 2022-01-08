1560c7e4361e1171d0eef38826908890925de37d
Robert <gdx147658@student.wsb.gda.pl>
2022-01-08 14:55:56 +0100

    ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,34 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        System.out.println("Zad 1");
+        System.out.println("\na)");
 
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0)
+                continue;
+            System.out.print(i + " ");
+        }
+        // continue powoduje przerwanie obecnego obiegu pętli i kontynuuje ją dalej zaczynając od kolejnego obiegu.
+
+        System.out.println("\nb)");
+
+        for (int i = 0; i <= 10; i++) {
+            if (i == 5)
+                break;
+            System.out.print(i + " ");
+        }
+        // break całkowicie zatrzymuje pętle i z niej wychodzi, nie jest ona dalej wykonywana,
+        // ale nie wychodzi z metody i wykonuje dalej kod.
+
+        System.out.println("\nc)");
+
+        for (int i = 0; i <= 10; i++) {
+            if (i == 7)
+                return;
+            System.out.print(i + " ");
+        }
+        // return również kończy pętle i w niech wychodzi, ale dodatkowo kończy działanie metody.
+        // np. gdybyśmy ten przykład dali jako b) to c) nigdy byśmy nie zobaczyli
     }
 }
