fadc2b47c862d3fdbb91953c66f121d993d6de90
Robert <gdx147658@student.wsb.gda.pl>
2022-01-08 22:05:50 +0100

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,62 @@ import java.util.*;
 
 public class Main {
 
+    public  static  List<String> uzupelnij(List<String> lista, int ilosc)
+    {
+        System.out.println("*Uzupełnianie listy*");
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < ilosc; i++)
+        {
+            System.out.printf("Wpisz %d. zwierzę: ", i+1);
+            lista.add(scan.next());
+        }
+        return lista;
+    }
+
+    public  static  Set<Integer> uzupelnij(Set<Integer> set, int ilosc)
+    {
+        System.out.println("*Uzupełnianie Set-a*");
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < ilosc; i++)
+        {
+            System.out.printf("Wpisz %d. liczbę: ", i+1);
+            set.add(scan.nextInt());
+        }
+        return set;
+    }
+
+    public static void wypisz(List<String> list)
+    {
+        System.out.println("\n*Wypisanie listy*");
+        for (String el : list)
+        {
+            System.out.printf("%s, ", el);
+        }
+        System.out.println("");
+    }
+
+    public static void wypisz(Set<Integer> set)
+    {
+        System.out.println("\n*Wypisanie Set-a*");
+        for (int el : set)
+        {
+            System.out.printf("%d, ", el);
+        }
+        System.out.println("");
+    }
+
+    public static void usun(List<String> list, int ilosc)
+    {
+        int x = list.size();
+        System.out.printf("\n*Usuwanie %d elementów od końca*\n", ilosc);
+        for (int i = 0; i < ilosc; i++)
+        {
+            list.remove(x-1);
+            x--;
+        }
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
 
         /* ćw_6 Listy */
@@ -19,12 +75,40 @@ public class Main {
                 elementów listy
           */
 
+        System.out.println("zad 1");
+
+        List<String> zwierzeta = new ArrayList<>();
+
+        uzupelnij(zwierzeta, 5);
+        wypisz(zwierzeta);
+
+        usun(zwierzeta, 2);
+        uzupelnij(zwierzeta, 3);
+        wypisz(zwierzeta);
+        System.out.println("Wielkość listy = " + zwierzeta.size());
+
+        System.out.println("\n*Odwrotne sortowanie*");
+        Collections.sort(zwierzeta, Collections.reverseOrder());
+        wypisz(zwierzeta);
+
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
 
+        System.out.println("\nzad 2");
+
+        Set<Integer> liczby = new TreeSet<>();
+        uzupelnij(liczby, 10);
+
+        System.out.println("*Wypisanie wyświetleniem Set-a*");
+        System.out.println(liczby);
+
+        System.out.println("\n*Wypisanie za pomoca forEach-em*");
+        wypisz(liczby);
+
+        //Z moich obserwacji wynika, że TreeSet sam sortuje elementy
     }
 
 }
